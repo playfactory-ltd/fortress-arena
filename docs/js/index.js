a802973bad8011281867358065bac475b41ed2d1
sehee Chang <sehee@atomrigs.io>
2022-01-08 18:37:42 +0900

    -. remove timer

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,7 @@
 const setTheme = (theme) => (document.documentElement.className = theme);
 setTheme("aquamarine"); // initialize
 bgImageScale();
-countDownTimer("countdown", "2022-01-06T13:00:00.000Z");
+// countDownTimer("countdown", "2022-01-06T13:00:00.000Z");
 // countDownTimer("countdown", "01/06/2022 12:33 PM");
 // let nftAddress = "0xD732C56BC9008272D780F339f97de79089c34f4B"; // rinkeby
 let nftContract;
@@ -66,7 +66,7 @@ function watchChainAccount() {
 
 async function startApp() {
   console.log("startApp");
-  clearInterval(totalsupplyInterval);
+  // clearInterval(totalsupplyInterval);
   try {
     var currentChainId = await web3.eth.getChainId();
     chainId = currentChainId;
@@ -193,7 +193,7 @@ async function getMintingState() {
 }
 
 async function getTotalSupply() {
-  clearInterval(totalsupplyInterval);
+  // clearInterval(totalsupplyInterval);
 
   let maxCnt = 0;
   let mintedCnt = 0;
@@ -203,18 +203,18 @@ async function getTotalSupply() {
   $(".claimedcnt").html(mintedCnt + "/" + maxCnt);
 
   // update every 2sec
-  totalsupplyInterval = setInterval(async function () {
-    mintedCnt = await nftContract.methods.getCurrentPublicId().call();
-    // console.log("mintedCnt =>", mintedCnt);
-    maxCnt = await checkMintingState(mintedCnt);
-    // console.log("maxCnt => ", maxCnt);
-    if (parseInt(mintedCnt) < parseInt(maxCnt)) {
-      $(".claimedcnt").html(mintedCnt + "/" + maxCnt);
-    } else {
-      $(".claimedcnt").html(mintedCnt + "/" + maxCnt);
-      clearInterval(totalsupplyInterval);
-    }
-  }, 2000);
+  // totalsupplyInterval = setInterval(async function () {
+  //   mintedCnt = await nftContract.methods.getCurrentPublicId().call();
+  //   // console.log("mintedCnt =>", mintedCnt);
+  //   maxCnt = await checkMintingState(mintedCnt);
+  //   // console.log("maxCnt => ", maxCnt);
+  //   if (parseInt(mintedCnt) < parseInt(maxCnt)) {
+  //     $(".claimedcnt").html(mintedCnt + "/" + maxCnt);
+  //   } else {
+  //     $(".claimedcnt").html(mintedCnt + "/" + maxCnt);
+  //     clearInterval(totalsupplyInterval);
+  //   }
+  // }, 2000);
 }
 
 async function checkMintingState(_mintedCnt) {
